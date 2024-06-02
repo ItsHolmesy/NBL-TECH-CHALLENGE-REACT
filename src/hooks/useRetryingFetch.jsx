ead4b30c15e574c716d29461e53a27b60b1301e2
ItsHolmesy <matt@yellowtwist.net>
2024-06-02 23:35:54 +1000

    Update useRetryingFetch.jsx

--- src/hooks/useRetryingFetch.jsx
+++ src/hooks/useRetryingFetch.jsx
@@ -1,35 +1,44 @@
 import { useState, useEffect } from "react"
+
+// Helper function to set delay.
 function sleep(timeMS) {
   return new Promise((resolve) => setTimeout(resolve, timeMS))
 }
 
+// Custom fetch hook including retry logic.
 const useRetryingFetch = (url, options, retryTimes = 3, retryTimeMS = 300) => {
   const [isLoading, setIsLoading] = useState(false)
   const [result, setResult] = useState(null)
   const [error, setError] = useState(null)
 
+  // Fetch data function.
   const fetchRetry = async (retriesRemain = retryTimes) => {
+    // Nested retry function.
     async function retry(error, retriesLeft) {
       if (!retriesLeft) {
         setIsLoading(false)
         setError(error)
       }
-
+      // Wait if error, retry after delay.
       return sleep(retryTimeMS).then(() => fetchRetry(--retriesRemain))
     }
     
     setIsLoading(true)
+
+    // Fetch Data from API
     const data = await fetch(url, options)
       .then(async (response) => await response.json())
-      .catch((error) => retry(error, retryTimes))
-    setIsLoading(false)
-    setResult(data)
+      .catch((error) => retry(error, retryTimes)) // Retry if error
+
+    setIsLoading(false) // Set loading to false after fetch is completed 
+    setResult(data) // Store fetched data
   }
 
   useEffect(() => {
     fetchRetry()
   }, [url, options])
 
+  // Return fetched data, loading, or errors
   return [result, isLoading, error]
 }
   
